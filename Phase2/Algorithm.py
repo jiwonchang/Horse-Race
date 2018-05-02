36485934cfb691b071de7cb44661c1358d4a0ea9
Ji Won Chang <jiwonchang@berkeley.edu>
2018-05-01 22:30:21 -0700

    First Commit

---
+++ Phase2/Algorithm.py
@@ -0,0 +1,12 @@
+#file = open('3.in')
+#print(file.read())
+
+#content = []
+
+#with open('3.in') as file:
+#    content = file.readlines()
+
+#print(content[1])
+
+with open('3.in') as file:
+    for line in file.readlines()
